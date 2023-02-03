4af4f78f677374e47c0a630f6f93922a562e28da
The Greatest Giant <seanalter1@gmail.com>
2023-02-03 15:16:25 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 npm install font-awesome-filetypes marked
+sleep 1
+npm audit fix --force
+sleep 1
+npm install font-awesome-filetypes marked
+
 rm startup.sh
+
+read -p "Do you want to auto-publish? (y/n) (Y)" pub
+if [ $pub != n ]
+then
+  wrangler publish
+fi
