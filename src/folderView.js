61de6b7af046cfbfa859b0256a3004782117f3ae
The Greatest Giant <seanalter1@gmail.com>
2023-02-03 15:59:34 -0500

    Update folderView.js

--- src/folderView.js
+++ src/folderView.js
@@ -110,10 +110,10 @@ export async function renderFolderView(items, path, request) {
             }
           })
           .join('')
-      )  +
+      )
+    ) +
     (readmeExists && !isIndex ? await renderMarkdown(readmeFetchUrl, 'fade-in-fwd', '') : '') +
     (isIndex ? intro : '')
-    ) 
   )
   return renderHTML(body, ...[request.pLink, request.pIdx])
 }
